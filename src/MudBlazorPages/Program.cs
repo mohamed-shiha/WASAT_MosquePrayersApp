15992e1d923499a778bbaa097ef4aa1f30df15d8
Mohamed Shiha <mohamedshiha89@gmail.com>
2024-08-27 11:57:42 +0100

    Initial commit

---
+++ src/MudBlazorPages/Program.cs
@@ -0,0 +1,16 @@
+using Blazored.LocalStorage;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<MudBlazorPages.App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddMudServices();
+
+builder.Services.AddBlazoredLocalStorage();
+
+await builder.Build().RunAsync();
